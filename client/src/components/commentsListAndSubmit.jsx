6b22f17797d8b6d5d79c6cbd0ee46b7279add25d
baklavacado <benny.l.wu@gmail.com>
2018-07-28 06:30:26 -0700

    use ajax get request to render a sample comment from api endpoint

---
+++ client/src/components/commentsListAndSubmit.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import $ from 'jquery';
+
+class CommentsList extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    };
+  }
+
+  componentDidMount () {
+    this.getComments();
+  }
+
+  getComments () {
+    $.get('api/songs/songId/comments', (data) => {
+      this.setState({ 
+        data: data, 
+        username: data[0].username, 
+        text: data[0].text
+      });
+      console.log('this array should be printed in chrome dev console', this.state.data)
+    });
+  }
+
+  render () {
+    return (
+      <div>
+        {this.state.username} - {this.state.text}
+      </div>
+    );
+  }
+}
+
+export default CommentsList; 
