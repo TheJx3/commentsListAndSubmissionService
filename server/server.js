f780139e59d8e52541d5fc1884a2cb65f58a553c
baklavacado <benny.l.wu@gmail.com>
2018-08-01 13:58:22 -0700

    install cors

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const express = require('express');
 const path = require('path');
+const cors = require('cors');
 const mongoDB = require('../db/db.js');
 
 const app = express();
+
+app.use(cors());
 
 app.use((req, res, next) => {
   console.log(req.method, req.path);
