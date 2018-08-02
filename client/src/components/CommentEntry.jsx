61345a2024181fb8984971017a7b2e9821820890
baklavacado <benny.l.wu@gmail.com>
2018-08-02 00:27:08 -0700

    attempt to convert styled components to CSS modules

--- client/src/components/CommentEntry.jsx
+++ client/src/components/CommentEntry.jsx
@@ -1,64 +1,65 @@
 import React from 'react';
-import styled from 'styled-components'; 
+// import styled from 'styled-components'; 
+import styles from '../css-modules/entries.css';
 import RepliesList from './RepliesList';
 import ReactTimeAgo from 'react-time-ago';
 
 // styles
-const CommentContainer = styled.div`
-  height: auto;
-  width: auto;
-  display: flex;
-  flex-direction: column;
-  margin: 10px 0px 20px 0px;
-  `;
+// const CommentContainer = styled.div`
+//   height: auto;
+//   width: auto;
+//   display: flex;
+//   flex-direction: column;
+//   margin: 10px 0px 20px 0px;
+//   `;
   
-const CommentContent = styled.div`
-  display: flex;
-  flex-direction: row;
-  width: auto;
-  margin-left: 10px;
-`;
+// const CommentContent = styled.div`
+//   display: flex;
+//   flex-direction: row;
+//   width: auto;
+//   margin-left: 10px;
+// `;
 
-const Avatar = styled.div`
-  height: 40px;
-  border: 2px solid cyan; 
-  flex-basis: 5%;
-`;
+// const Avatar = styled.div`
+//   height: 40px;
+//   border: 2px solid cyan; 
+//   flex-basis: 5%;
+// `;
   
-const Content = styled.div`
-  display: flex;
-  flex-direction: column;
-  font-size: 12px;
-  margin: 0 0 30px 10px;
-  flex-basis: 80%;
-`;
+// const Content = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   font-size: 12px;
+//   margin: 0 0 30px 10px;
+//   flex-basis: 80%;
+// `;
 
-const CommentUser = styled.div`
-  color: #ccc;
-`;
+// const CommentUser = styled.div`
+//   color: #ccc;
+// `;
 
-const CommentText = styled.div`
-  color: black;
-`;
+// const CommentText = styled.div`
+//   color: black;
+// `;
 
-const RightContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  flex-basis: 15%;
-  font-size: 12px;
-  margin: 0px 10px 0px 0px;
-  text-align: right; 
-  color: #999;
-`;
+// const RightContainer = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   flex-basis: 15%;
+//   font-size: 12px;
+//   margin: 0px 10px 0px 0px;
+//   text-align: right; 
+//   color: #999;
+// `;
 
-const ReplyButton = styled.button`
-  height: 28px; 
-  width: 32px;
-  color: #999; 
-  margin-left: 100px;
-`
+// const ReplyButton = styled.button`
+//   height: 28px; 
+//   width: 32px;
+//   color: #999; 
+//   margin-left: 100px;
+// `
+
 // components
-
 const CommentEntry = ({username, songtime, text, timestamp, replies}) => {
   const secondsToTime = (songTime) => {
     songTime = Math.floor(songTime);
@@ -78,32 +79,32 @@ const CommentEntry = ({username, songtime, text, timestamp, replies}) => {
   }
 
   return (
-    <CommentContainer>
-      <CommentContent>
-      <Avatar>
+    <div className={styles.entriesContainer}>
+      <div className={styles.content}>
+      <div className={styles.avatar}>
         <a href='#'>
           <i className="fas fa-user-circle fa-2x"/>
         </a>
-      </Avatar>
-      <Content>
-        <CommentUser>
+      </div>
+      <div className={styles.commentContent}>
+        <div className={styles.user}>
           <a href='#'>{username}</a> at <a href='#'>{secondsToTime(songtime)}</a>
-        </CommentUser>
-        <CommentText>
+        </div>
+        <div className={styles.text}>
           {text}
-        </CommentText>
-      </Content>
-      <RightContainer>
+        </div>
+      </div>
+      <div className={styles.rightContainer}>
           <ReactTimeAgo>
             {timestamp}
           </ReactTimeAgo>
-          <ReplyButton>
+          <div className={styles.replyButton}>
             <i className="fas fa-reply fa-lg" />
-          </ReplyButton>
-      </RightContainer>  
-      </CommentContent>
+          </div>
+      </div>  
+      </div>
       <RepliesList repliesList={replies}/>
-    </CommentContainer>
+    </div>
   )
 };
 
