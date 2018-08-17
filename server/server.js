d4766a954681fcaf591d9533dc4a5cfc4b61c57b
Jordan Bice <jmbice@gmail.com>
2018-08-16 20:04:30 -0700

    Add api's to server for new DB calls

--- server/server.js
+++ server/server.js
@@ -1,17 +1,17 @@
 const express = require('express');
 const path = require('path');
 const cors = require('cors');
+const cassandra = require('./dbFunctions/dbHelpers.js');
 // const mongoDB = require('../db/db.js');
-const db = require('./dbFunctions/index.js')
-
+const PORT = 1458;
 const app = express();
+
 app.use(cors());
 app.use((req, res, next) => {
   console.log(req.method, req.path);
   next();
 });
 
-// serve static assets
 app.use(express.static(path.join(__dirname, '../client/dist')));
 
 // outputs all comments for a song
@@ -25,14 +25,99 @@ app.use(express.static(path.join(__dirname, '../client/dist')));
 //   });
 // });
 
-const query = 'SELECT name, email FROM users WHERE key = ?';
+app.get('/songs/:id/comment', (req, res) => {
+  cassandra.getAll(req.params.id, (err, success) => {
+    if (err) {
+      console.log('Error at the server level with getAll!', err);
+      res.status(404).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
 
-client.execute(query, [ 'someone' ], function(err, result) {
-  assert.ifError(err);
-  console.log('User with email %s', result.rows[0].email);
+app.post('/songs/:id/comment', (req, res) => {
+  //comment needs to be an object with fields: song_id, comment_id, reply_id, comment_author, comment_avatar, comment_timestamp, comment_text, reply_author, reply_avatar, reply_timestamp, reply_text
+  cassandra.addCommentOrReply(req.body.comment, (err, success) => {
+    if (err) {
+      console.log('Error at the server level with add comment!!', err);
+      res.status(404).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
+
+app.post('/songs/:id/reply/:comment_id/', (req, res) => {
+  //reply needs to be an object with fields: song_id, comment_id, reply_id, comment_author, comment_avatar, comment_timestamp, comment_text, reply_author, reply_avatar, reply_timestamp, reply_text
+  cassandra.addCommentOrReply(req.body.reply, (err, success) => {
+    if (err) {
+      console.log('Error at the server level with add reply!!', err);
+      res.status(404).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
 });
 
 
+app.put('/songs/:id/comment/:comment_id/', (req, res) => {
+  //commentEdit needs to be an object with fields: comment_text, song_id, comment_id
+  cassandra.editComment(req.body.commentEdit, (err, success) => {
+    if (err) {
+      console.log('Error at the server level with edit comment!!', err);
+      res.status(404).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
+
+app.put('songs/:id/reply/:comment_id/:reply_id/', (req, res) => {
+  //commentEdit needs to be an object with fields: reply_text, song_id, comment_id, reply_id
+  cassandra.editReply(req.body.replyEdit, (err, success) => {
+    if (err) {
+      console.log('Error at the server level with edit reply!!', err);
+      res.status(404).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
+
+app.delete('.songs/:id/comment/', (req, res) => {
+  cassandra.deleteSong(req.params.id, (err, success) => {
+    if (err) {
+      console.log('Error at the server level, failed to delete all song comments!', err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
+
+app.delete('.songs/:id/comment/:comment_id', (req, res) => {
+  cassandra.deleteSong({ song_id: req.params.id, comment_id: req.params.comment_id }, (err, success) => {
+    if (err) {
+      console.log('Error at the server level, failed to delete a comment!', err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
+
+
+app.delete('songs/:id/reply/:comment_id/:reply_id', (req, res) => {
+  cassandra.deleteSong({ song_id: req.params.id, comment_id: req.params.comment_id, reply_id: req.params.reply_id }, (err, success) => {
+    if (err) {
+      console.log('Error at the server level, failed to delete a reply!', err);
+      res.status(400).send();
+    } else {
+      res.status(200).send(success);
+    }
+  });
+});
+
 // server connection
-const PORT = 1458;
 app.listen(PORT, () => console.log(`Listening on ${PORT}`));
